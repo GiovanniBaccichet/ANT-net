250d578409710817106b563a4587f3989c19eda2
GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>
2024-10-10 11:09:03 +0200

    Generate user & API token script
    Signed-off-by: GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>

---
+++ scripts/generate_api_token.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pveum user add terraform@pve && \
+pveum role add Terraform -privs "Datastore.Allocate Datastore.AllocateSpace Datastore.AllocateTemplate Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify SDN.Use VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt User.Modify" && \
+pveum aclmod / -user terraform@pve -role Terraform && \
+pveum user token add terraform@pve provider --privsep=0
