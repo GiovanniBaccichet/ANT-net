12600258cbb904561a9a0f574aa9da2958410f20
GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>
2024-10-11 10:56:59 +0200

    Added network aliases module
    Signed-off-by: GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>

---
+++ terraform/modules/firewall_aliases/variables.tf
@@ -0,0 +1,11 @@
+variable "alias_name" {
+  description = "Network Alias Name"
+}
+
+variable "alias_cidr" {
+  description = "CIDR"
+}
+
+variable "alias_comment" {
+  description = "Comment for Network Alias"
+}
