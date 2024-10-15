d4f491b2da3933c128e48404b4447da368a94253
GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>
2024-10-15 15:16:40 +0200

    API Name fix
    Signed-off-by: GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>

--- scripts/ssh_api_token_setup.sh
+++ scripts/ssh_api_token_setup.sh
@@ -69,10 +69,10 @@ fi
 
 # Execute commands on the Proxmox server
 output=$(ssh -i "$SSH_PRIVATE_KEY" -T root@"$PROXMOX_IP" << 'EOF'
-pveum user add terraform33@pve && \
-pveum role add Terraform33 -privs "Datastore.Allocate Datastore.AllocateSpace Datastore.AllocateTemplate Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify SDN.Use VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt User.Modify" && \
-pveum aclmod / -user terraform33@pve -role Terraform33 && \
-pveum user token add terraform33@pve provider --privsep=0
+pveum user add terraform@pve && \
+pveum role add Terraform -privs "Datastore.Allocate Datastore.AllocateSpace Datastore.AllocateTemplate Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify SDN.Use VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt User.Modify" && \
+pveum aclmod / -user terraform@pve -role Terraform && \
+pveum user token add terraform@pve provider --privsep=0
 EOF
 )
 
