3418248e1221b902cb265cf0fc2a3f8917e0a0ad
GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>
2024-10-16 11:40:45 +0200

    Fixed DHCP IPAM problem
    Signed-off-by: GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>

--- README.md
+++ README.md
@@ -98,7 +98,7 @@ The script can be found in `scripts/network_setup.sh`, but here is a short comme
 
 1. Create a **simple zone**: 
    ```bash
-   pvesh create /cluster/sdn/zones --type simple --zone "labnet" --dhcp dnsmasq
+   pvesh create /cluster/sdn/zones --type simple --zone "labnet" --dhcp "dnsmasq" --ipam "pve"
    ```
 2. Create a **Virtual Network** within the previously created zone:
    ```bash
--- scripts/network_setup.sh
+++ scripts/network_setup.sh
@@ -13,7 +13,7 @@ echo "[!] Installing dnsmasq"
 apt install -y dnsmasq && systemctl disable --now dnsmasq
 
 echo "[!] Creating Zone"
-pvesh create /cluster/sdn/zones --type simple --zone $ZONE_NAME --dhcp dnsmasq && \
+pvesh create /cluster/sdn/zones --type simple --zone $ZONE_NAME --dhcp "dnsmasq" --ipam "pve" && \
 
 echo "[!] Creating Virtual Network"
 pvesh create /cluster/sdn/vnets --vnet $VNET_NAME --zone $ZONE_NAME && \
