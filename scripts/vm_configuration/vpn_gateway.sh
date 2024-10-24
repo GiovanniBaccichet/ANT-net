7f14dc07ac2e451a0dbb0fcc1b2d808b036f3f4a
GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>
2024-10-24 11:59:19 +0200

    Install VPN server
    Signed-off-by: GiovanniBaccichet <32038133+GiovanniBaccichet@users.noreply.github.com>

--- scripts/vm_configuration/vpn_gateway.sh
+++ scripts/vm_configuration/vpn_gateway.sh
@@ -14,5 +14,9 @@ sleep 300 && \
 
 qm guest exec 110 -- bash -c 'echo -e "network:\n  version: 2\n  renderer: networkd\n  ethernets:\n    ens16:\n      dhcp4: true" | sudo tee /etc/netplan/99_config.yaml > /dev/null && sudo netplan apply' && \
 
+sleep 10 && \
+
+qm guest exec 110 -- bash -c 'sudo apt-get update -y && sudo apt install wireguard-tools net-tools --no-install-recommends -y && git clone https://github.com/donaldzou/WGDashboard.git && cd ./WGDashboard/src && chmod +x ./wgd.sh && ./wgd.sh install && sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf && sudo sysctl -p /etc/sysctl.conf && sudo iptables -A FORWARD -i wg0 -o eth0 -j ACCEPT && sudo iptables -A FORWARD -i eth0 -o wg0 -m state --state ESTABLISHED,RELATED -j ACCEPT && sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE && ./wgd.sh start' && \
+
 echo "Done"
 
